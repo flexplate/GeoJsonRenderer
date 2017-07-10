30d9f9df87bbad765c67f367bfe54c6215c0167e
Daniel Price <danielprice188@hotmail.com>
2017-07-10 15:31:57 +0100

    Lots of stuff with extents

--- GeoJsonRenderer/Envelope.cs
+++ GeoJsonRenderer/Envelope.cs
@@ -1,50 +1,139 @@
 ﻿using GeoJSON.Net;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace Therezin.GeoJsonRenderer
 {
-    public class Envelope
-    {
-        public double MinX { get; set; }
-        public double MinY { get; set; }
-        public double MaxX { get; set; }
-        public double MaxY { get; set; }
+	public class Envelope
+	{
+		public double MinX { get; set; }
+		public double MinY { get; set; }
+		public double MaxX { get; set; }
+		public double MaxY { get; set; }
 
-        public Envelope() { }
+		public Envelope() { }
 
-        public Envelope(double minX, double minY, double maxX, double maxY)
-        {
-            MinX = minX;
-            MinY = minY;
-            MaxX = maxX;
-            MaxY = maxY;
-        }
+		public Envelope(double minX, double minY, double maxX, double maxY)
+		{
+			MinX = minX;
+			MinY = minY;
+			MaxX = maxX;
+			MaxY = maxY;
+		}
 
-        private static Envelope FindExtents(List<GeoJSONObject> geoJsonObjects)
-        {
-            var Extents = new Envelope();
-            foreach (GeoJSONObject Item in geoJsonObjects)
-            {
-                if (Item.BoundingBoxes != null)
-                {
-                    if (Item.BoundingBoxes.Length == 4)    // -X, -Y, +X, +Y
-                    {
-                        if (Item.BoundingBoxes[0] < Extents.MinX) { Extents.MinX = Item.BoundingBoxes[0]; }
-                        if (Item.BoundingBoxes[1] < Extents.MinY) { Extents.MinY = Item.BoundingBoxes[1]; }
-                        if (Item.BoundingBoxes[2] > Extents.MaxX) { Extents.MaxX = Item.BoundingBoxes[2]; }
-                        if (Item.BoundingBoxes[3] > Extents.MaxY) { Extents.MaxY = Item.BoundingBoxes[3]; }
-                    }
-                    else if (Item.BoundingBoxes.Length == 6) // -X, -Y, -Z, +X, +Y, +Z
-                    {
-                        if (Item.BoundingBoxes[0] < Extents.MinX) { Extents.MinX = Item.BoundingBoxes[0]; }
-                        if (Item.BoundingBoxes[1] < Extents.MinY) { Extents.MinY = Item.BoundingBoxes[1]; }
-                        if (Item.BoundingBoxes[3] > Extents.MaxX) { Extents.MaxX = Item.BoundingBoxes[3]; }
-                        if (Item.BoundingBoxes[3] > Extents.MaxY) { Extents.MaxY = Item.BoundingBoxes[4]; }
-                    }
-                }
-            }
-            return Extents;
-        }
 
-    }
+		public static Envelope FindExtents(List<IGeometryObject> geoJsonObjects, Envelope extents = null)
+		{
+			var Extents = new Envelope();
+			if (extents != null)
+			{
+				Extents = extents;
+			}
+
+			foreach (GeoJSONObject Item in geoJsonObjects)
+			{
+				if (Item.BoundingBoxes != null)
+				{
+					if (Item.BoundingBoxes.Length == 4)    // -X, -Y, +X, +Y
+					{
+						if (Item.BoundingBoxes[0] < Extents.MinX) { Extents.MinX = Item.BoundingBoxes[0]; }
+						if (Item.BoundingBoxes[1] < Extents.MinY) { Extents.MinY = Item.BoundingBoxes[1]; }
+						if (Item.BoundingBoxes[2] > Extents.MaxX) { Extents.MaxX = Item.BoundingBoxes[2]; }
+						if (Item.BoundingBoxes[3] > Extents.MaxY) { Extents.MaxY = Item.BoundingBoxes[3]; }
+					}
+					else if (Item.BoundingBoxes.Length == 6) // -X, -Y, -Z, +X, +Y, +Z
+					{
+						if (Item.BoundingBoxes[0] < Extents.MinX) { Extents.MinX = Item.BoundingBoxes[0]; }
+						if (Item.BoundingBoxes[1] < Extents.MinY) { Extents.MinY = Item.BoundingBoxes[1]; }
+						if (Item.BoundingBoxes[3] > Extents.MaxX) { Extents.MaxX = Item.BoundingBoxes[3]; }
+						if (Item.BoundingBoxes[3] > Extents.MaxY) { Extents.MaxY = Item.BoundingBoxes[4]; }
+					}
+				}
+				else
+				{
+					switch (Item.Type)
+					{
+						case GeoJSONObjectType.Point:
+							Extents = FindExtents(((Point)Item).Coordinates, Extents);
+							break;
+						case GeoJSONObjectType.MultiPoint:
+							foreach (var Point in ((MultiPoint)Item).Coordinates) { Extents = FindExtents(Point.Coordinates, Extents); }
+							break;
+						case GeoJSONObjectType.LineString:
+							foreach (var Position in ((LineString)Item).Coordinates) { Extents = FindExtents(Position, Extents); }
+
+							break;
+						case GeoJSONObjectType.MultiLineString:
+							foreach (var Line in ((MultiLineString)Item).Coordinates)
+							{
+								foreach (var Position in Line.Coordinates) { Extents = FindExtents(Position, Extents); }
+							}
+							break;
+						case GeoJSONObjectType.Polygon:
+							foreach (var Line in ((Polygon)Item).Coordinates)
+							{
+								foreach (var Position in Line.Coordinates) { Extents = FindExtents(Position, Extents); }
+							}
+							break;
+						case GeoJSONObjectType.MultiPolygon:
+							foreach (var Poly in ((MultiPolygon)Item).Coordinates)
+							{
+								foreach (var Line in Poly.Coordinates)
+								{
+									foreach (var Position in Line.Coordinates) { Extents = FindExtents(Position, Extents); }
+								}
+							}
+							break;
+						case GeoJSONObjectType.GeometryCollection:
+							Extents = FindExtents(((GeometryCollection)Item).Geometries, Extents);
+							break;
+						case GeoJSONObjectType.Feature:
+							FeatureCollection Collection = new FeatureCollection(new List<Feature>() { (Feature)Item });
+							Extents = FindExtents(Collection, Extents);
+							break;
+						case GeoJSONObjectType.FeatureCollection:
+							Extents = FindExtents((FeatureCollection)Item, Extents);
+							break;
+						default:
+							break;
+					}
+				}
+			}
+			return Extents;
+		}
+
+		public static Envelope FindExtents(FeatureCollection features, Envelope extents = null)
+		{
+			var Extents = new Envelope();
+			if (extents != null)
+			{
+				Extents = extents;
+			}
+
+			var Geometries = new List<IGeometryObject>();
+			foreach (var Feature in features.Features)
+			{
+				Geometries.Add(Feature.Geometry);
+			}
+			return FindExtents(Geometries, Extents);
+		}
+
+
+		private static Envelope FindExtents(IPosition position, Envelope extents = null)
+		{
+			var Extents = new Envelope();
+			if (extents != null)
+			{
+				Extents = extents;
+			}
+			Extents.MinX = new double[] { Extents.MinX, position.Latitude }.Where(v => v != 0).Min();
+			Extents.MinY = new double[] { Extents.MinY, position.Longitude }.Where(v => v != 0).Min();
+			Extents.MaxX = Math.Max(Extents.MaxX, position.Latitude);
+			Extents.MaxY = Math.Max(Extents.MaxY, position.Longitude);
+			return Extents;
+		}
+	}
 }
--- GeoJsonRenderer/GeoJsonRenderer.cs
+++ GeoJsonRenderer/GeoJsonRenderer.cs
@@ -12,126 +12,195 @@ using GeoJSON.Net.Feature;
 
 namespace Therezin.GeoJsonRenderer
 {
-    public class GeoJsonRenderer
-    {
-        private Bitmap Bitmap;
-        private Graphics Graphics;
+	public class GeoJsonRenderer
+	{
+		private Bitmap Bitmap;
+		private Graphics Graphics;
 
-        public bool RenderGeoJson(string json, string outputPath, int width, int height)
-        {
-            var GeoJsonObjects = JsonConvert.DeserializeObject<FeatureCollection>(json);
+		public bool RenderGeoJson(string json, string outputPath, int width, int height)
+		{
+			var GeoJsonObjects = JsonConvert.DeserializeObject<FeatureCollection>(json);
 
-            // Find envelope of geometry
-            Envelope Extents = Envelope.FindExtents(GeoJsonObjects);
+			// Find envelope of geometry
+			Envelope Extents = Envelope.FindExtents(GeoJsonObjects);
 
-            // Rebase to zero
-            RebaseGeometry(GeoJsonObjects, Extents);
+			// Rebase to zero
+			RebaseGeometry(GeoJsonObjects, Extents);
 
-            // Zoom to extents
+			// Zoom to extents
+			Envelope Extents2 = Envelope.FindExtents(GeoJsonObjects);
 
-            // Create canvas
-            Bitmap = new Bitmap(width, height);
-            Graphics = Graphics.FromImage(Bitmap);
-            Graphics.FillRectangle(Brushes.White, new Rectangle(0, 0, width, height));
+			// Create canvas
+			using (Bitmap = new Bitmap(width, height))
+			{
+				using (Graphics = Graphics.FromImage(Bitmap))
+				{
 
-            // Draw [TEST, REDO.]
-            foreach (var Item in GeoJsonObjects.Features)
-            {
-                DrawGeometry(Item.Geometry);
-            }
+					Graphics.FillRectangle(Brushes.White, new Rectangle(0, 0, width, height));
 
-            // Save
-            Bitmap.Save(outputPath);
-            return true;
-        }
+					// Draw [TEST, REDO.]
+					foreach (var Item in GeoJsonObjects.Features)
+					{
+						DrawGeometry(Item.Geometry);
+					}
 
-        /// <summary>
-        /// Iterate through feature collection and rebase its coordinates around zero.
-        /// </summary>
-        /// <param name="features"></param>
-        /// <param name="envelope"></param>
-        private void RebaseGeometry(FeatureCollection features, Envelope envelope)
-        {
-            foreach (Feature Feature in features.Features)
-            {
-                
-            }
-        }
+					// Save
+					Bitmap.Save(outputPath);
+					return true;
+				}
+			}
 
-        /// <summary>
-        /// Recursively draw a geometry object.
-        /// </summary>
-        private void DrawGeometry(IGeometryObject geometry)
-        {
-            switch (geometry.Type)
-            {
-                case GeoJSONObjectType.Point:
-                    var Position = ((GeoJSON.Net.Geometry.Point)geometry).Coordinates;
-                    PointF PtF = new PointF() { X = (float)Position.Latitude, Y = (float)Position.Longitude };
-                    Graphics.DrawLine(StyleChooser(), PtF, PtF);
-                    break;
-                case GeoJSONObjectType.MultiPoint:
-                    break;
-                case GeoJSONObjectType.LineString:
-                    Graphics.DrawLines(StyleChooser(), ConvertPositionsToPoints(((LineString)geometry).Coordinates));
-                    break;
-                case GeoJSONObjectType.MultiLineString:
-                    foreach (var Line in ((MultiLineString)geometry).Coordinates)
-                    {
-                        DrawGeometry(Line);
-                    }
-                    break;
-                case GeoJSONObjectType.Polygon:
-                    foreach (var PolyLine in ((Polygon)geometry).Coordinates)
-                    {
-                        Graphics.DrawPolygon(StyleChooser(), ConvertPositionsToPoints(PolyLine.Coordinates));
-                    }
-                    break;
-                case GeoJSONObjectType.MultiPolygon:
-                    break;
-                case GeoJSONObjectType.GeometryCollection:
-                    foreach (var Geo in ((GeometryCollection)geometry).Geometries)
-                    {
-                        DrawGeometry(Geo);
-                    }
-                    break;
-                case GeoJSONObjectType.Feature:
-                    break;
-                case GeoJSONObjectType.FeatureCollection:
-                    break;
-                default:
-                    break;
-            }
+		}
 
-        }
-                
-                private PointF[] ConvertPositionsToPoints(List<IPosition> positions)
-        {
-            var OutList = new List<PointF>();
-            for (int i = 0; i < positions.Count; i++)
-            {
-                OutList.Add(new PointF() { X = (int)positions[i].Latitude, Y = (int)positions[i].Longitude });
-            }
-            return OutList.ToArray();
-        }
+		/// <summary>
+		/// Iterate through feature collection and rebase its coordinates around zero.
+		/// </summary>
+		/// <param name="features"></param>
+		/// <param name="envelope"></param>
+		private void RebaseGeometry(FeatureCollection features, Envelope envelope)
+		{
+			for (int f = 0; f < features.Features.Count; f++)
+			{
+				Feature Feature = features.Features[f];
+				switch (Feature.Geometry.Type)
+				{
+					case GeoJSONObjectType.Point:
+						Feature = new Feature(new GeoJSON.Net.Geometry.Point(RebasePosition(((GeoJSON.Net.Geometry.Point)Feature.Geometry).Coordinates, envelope)), Feature.Properties, Feature.Id != null ? Feature.Id : null);
+						break;
+					case GeoJSONObjectType.MultiPoint:
+						{
+							var MultiP = ((MultiPoint)Feature.Geometry);
+							for (int i = 0; i < MultiP.Coordinates.Count; i++)
+							{
+								MultiP.Coordinates[i] = new GeoJSON.Net.Geometry.Point(RebasePosition(MultiP.Coordinates[i].Coordinates, envelope));
+							}
+							break;
+						}
+					case GeoJSONObjectType.LineString:
+						{
+							var Line = ((LineString)Feature.Geometry);
+							for (int i = 0; i < Line.Coordinates.Count; i++) { ((LineString)Feature.Geometry).Coordinates[i] = RebasePosition(Line.Coordinates[i], envelope); }
+							break;
+						}
+					case GeoJSONObjectType.MultiLineString:
+						foreach (var Line in ((MultiLineString)Feature.Geometry).Coordinates)
+						{
+							for (int i = 0; i < Line.Coordinates.Count; i++)
+							{ Line.Coordinates[i] = RebasePosition(Line.Coordinates[i], envelope); }
+						}
+						break;
+					case GeoJSONObjectType.Polygon:
+						foreach (var Line in ((Polygon)Feature.Geometry).Coordinates)
+						{
+							for (int i = 0; i < Line.Coordinates.Count; i++)
+							{ Line.Coordinates[i] = RebasePosition(Line.Coordinates[i], envelope); }
+						}
+						break;
+					case GeoJSONObjectType.MultiPolygon:
+						foreach (var Poly in ((MultiPolygon)Feature.Geometry).Coordinates)
+						{
+							foreach (var Line in Poly.Coordinates)
+							{
+								for (int i = 0; i < Line.Coordinates.Count; i++)
+								{ Line.Coordinates[i] = RebasePosition(Line.Coordinates[i], envelope); }
+							}
+						}
+						break;
+					case GeoJSONObjectType.GeometryCollection:
+						// TODO
+						break;
+					case GeoJSONObjectType.Feature:
+						FeatureCollection Collection = new FeatureCollection(new List<Feature>() { Feature });
+						RebaseGeometry(Collection, envelope);
+						break;
+					case GeoJSONObjectType.FeatureCollection:
+						RebaseGeometry((FeatureCollection)Feature.Geometry, envelope);
+						break;
+					default:
+						break;
+				}
+			}
+		}
+
+		private IPosition RebasePosition(IPosition coordinates, Envelope envelope)
+		{
+			return new Position(coordinates.Longitude - envelope.MinX, coordinates.Latitude - envelope.MinY);
+		}
+
+		/// <summary>
+		/// Recursively draw a geometry object.
+		/// </summary>
+		private void DrawGeometry(IGeometryObject geometry)
+		{
+			switch (geometry.Type)
+			{
+				case GeoJSONObjectType.Point:
+					var Position = ((GeoJSON.Net.Geometry.Point)geometry).Coordinates;
+					PointF PtF = new PointF() { X = (float)Position.Longitude, Y = (float)Position.Latitude };
+					Graphics.DrawLine(StyleChooser(), PtF, PtF);
+					break;
+				case GeoJSONObjectType.MultiPoint:
+					break;
+				case GeoJSONObjectType.LineString:
+					Graphics.DrawLines(StyleChooser(), ConvertPositionsToPoints(((LineString)geometry).Coordinates));
+					break;
+				case GeoJSONObjectType.MultiLineString:
+					foreach (var Line in ((MultiLineString)geometry).Coordinates)
+					{
+						DrawGeometry(Line);
+					}
+					break;
+				case GeoJSONObjectType.Polygon:
+					foreach (var PolyLine in ((Polygon)geometry).Coordinates)
+					{
+						Graphics.DrawPolygon(StyleChooser(), ConvertPositionsToPoints(PolyLine.Coordinates));
+					}
+					break;
+				case GeoJSONObjectType.MultiPolygon:
+					break;
+				case GeoJSONObjectType.GeometryCollection:
+					foreach (var Geo in ((GeometryCollection)geometry).Geometries)
+					{
+						DrawGeometry(Geo);
+					}
+					break;
+				case GeoJSONObjectType.Feature:
+					break;
+				case GeoJSONObjectType.FeatureCollection:
+					break;
+				default:
+					break;
+			}
+
+		}
+
+		private PointF[] ConvertPositionsToPoints(List<IPosition> positions)
+		{
+			var OutList = new List<PointF>();
+			for (int i = 0; i < positions.Count; i++)
+			{
+				OutList.Add(new PointF() { X = (int)positions[i].Latitude, Y = (int)positions[i].Longitude });
+			}
+			return OutList.ToArray();
+		}
 
 
-        public static void TestCreateFile(string fullPath)
-        {
-            using (var Bmp = new Bitmap(400, 300))
-            {
-                Graphics G = Graphics.FromImage(Bmp);
-                G.FillRectangle(Brushes.White, new Rectangle(0, 0, 400, 300));
-                Pen GreenPen = new Pen(new SolidBrush(Color.Green), 2);
-                G.DrawLine(GreenPen, new System.Drawing.Point(30, 30), new System.Drawing.Point(300, 160));
-                Bmp.Save(fullPath);
-            }
-        }
+		public static void TestCreateFile(string fullPath)
+		{
+			using (var Bmp = new Bitmap(400, 300))
+			{
+				Graphics G = Graphics.FromImage(Bmp);
+				G.FillRectangle(Brushes.White, new Rectangle(0, 0, 400, 300));
+				Pen GreenPen = new Pen(new SolidBrush(Color.Green), 2);
+				G.DrawLine(GreenPen, new System.Drawing.Point(30, 30), new System.Drawing.Point(300, 160));
+				Bmp.Save(fullPath);
+			}
+		}
 
-        private Pen StyleChooser()
-        {
-            return new Pen(new SolidBrush(Color.Green), 2);
-        }
+		private Pen StyleChooser()
+		{
+			return new Pen(new SolidBrush(Color.Green), 2);
+		}
 
-    }
+	}
 }
